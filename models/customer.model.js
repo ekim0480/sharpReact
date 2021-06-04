230ef226a9146ecfcc09ee469296143c777e9d5d
EK <ekim0480@gmail.com>
2021-06-03 20:56:34 -0500

    first commit, functioning first/last name

---
+++ models/customer.model.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, DataTypes) {
+  const Customer = sequelize.define("customer", {
+    // Giving the Customer model structure
+    firstName: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      notEmpty: true,
+    },
+    lastName: {
+      type: DataTypes.STRING(20),
+      // allowNull: false,
+      notEmpty: true,
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
+      // defaultValue: "1111-11-11",
+      // allowNull: false,
+      validate: {
+        isDate: true,
+      },
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      // defaultValue: "1111111111",
+      // allowNull: false,
+      validate: {
+        isNumeric: true,
+        isAlphanumeric: true,
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING(30),
+      // defaultValue: "",
+      allowNull: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    mileage: {
+      type: DataTypes.STRING(20),
+      // defaultValue: "",
+      allowNull: true,
+    },
+  });
+
+  return Customer;
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependencies
+const express = require("express");
+const cors = require("cors");
+const path = __dirname + "/views/";
+
+// Initialize express app.
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+// Requiring in our models
+const db = require("./models");
+
+var corsOptions = {
+  origin: "http://localhost:3001",
+};
+
+app.use(cors(corsOptions));
+app.use(express.static(path));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path + "index.html");
+// });
+
+// routes
+require("./routes/customer.routes")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
